f2448b019e14f17f9cd82f896c5cbe9ce5ce4172
Mustafo Faiz <m.fayzov1@gmail.com>
2020-09-01 01:03:50 +0500

    ⚡ Initialized base eslint.js (#6)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
+};
