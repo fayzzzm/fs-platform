a011e2ca67e500e28de1134287ec6c6a24b59e05
fayzzz <mustafo.fayz@yandex.ru>
2020-09-02 22:42:17 +0500

    Completed code_check router, changed fetch endpoint and made change to utils

--- src/router/code-check/index.ts
+++ src/router/code-check/index.ts
@@ -1,16 +1,29 @@
 import * as express from 'express';
-import { codeCheck } from './code_check';
+import { codeCheck } from '../../utils/code_check';
+import multer from 'multer';
 
 export const router = express.Router();
 
-router.post('/', (req, res) => {
-    const { code } = req.body;
+router
+    .post('/', (req, res) => {
+        const { code } = req.body;
 
-    try {
-        const message = codeCheck(code);
-        res.send(message);
-    } catch (e) {
-        console.log(e);
-        res.status(400).send(e);
-    }
-});
+        try {
+            const message = codeCheck(code);
+            res.send(message);
+        } catch (e) {
+            res.status(400).send(e);
+        }
+    })
+    .post('/file', multer().single('file'), (req, res) => {
+        const { originalname, buffer } = req.file;
+
+        if (/main\.js/.test(originalname)) {
+            const code = buffer.toString();
+            const message = codeCheck(code);
+
+            res.send(message);
+        } else {
+            throw new Error('Wrong file name');
+        }
+    });
